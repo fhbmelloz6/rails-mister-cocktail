7c2b72c9255e53049226c38f942d6b6f7437ca8c
Francisco Mello <fhbmelloz6@gmail.com>
2017-08-13 22:29:46 -0300

    nova atualização

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "bom")
+Ingredient.create(name: "muito bom")
+Ingredient.create(name: "ótimo")
+Ingredient.create(name: "excelente")
+Ingredient.create(name: "bonito")
+Ingredient.create(name: "lindo")
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
